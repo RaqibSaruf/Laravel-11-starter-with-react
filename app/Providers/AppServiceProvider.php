506d3be07e3c318a0dddd4be49e01a0712a9db12
saruf <raqibhsan.2120@gmail.com>
2024-11-06 01:12:11 +0600

    update traits and others

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::before(function (User $user, $ability) {
-            return $user->hasRole(RolesEnum::SUPERADMIN->value) ? true : null;
+            return $user->hasRole([RolesEnum::SUPERADMIN->value, RolesEnum::ADMIN->value]) ? true : null;
         });
     }
 }
