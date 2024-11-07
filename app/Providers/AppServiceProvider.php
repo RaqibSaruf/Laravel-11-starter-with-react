a0f113f03a41178981581c4d3a3f8f9b7b7559f2
saruf <raqibhsan.2120@gmail.com>
2024-11-07 13:06:43 +0600

    update boot action and batch scopes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ namespace App\Providers;
 
 use App\Enums\RolesEnum;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -26,5 +27,7 @@ class AppServiceProvider extends ServiceProvider
         Gate::before(function (User $user, $ability) {
             return $user->hasRole([RolesEnum::SUPERADMIN->value, RolesEnum::ADMIN->value]) ? true : null;
         });
+
+        Model::shouldBeStrict();
     }
 }
